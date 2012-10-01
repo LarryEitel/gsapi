283d204abfc3e5cf820fbec4de2fa4aaf155d193
Larry Eitel <larry@eitel.com>
2012-10-01 10:22:01 -0600

    tweeking fabfile

--- fabfile.py
+++ fabfile.py
@@ -101,3 +101,50 @@ def restart():
 def pushpull():
     local('git push') # runs the command on the local environment
     run('cd /path/to/project/; git pull') # runs the command on the remote environment
+
+
+
+# the following is fab file from another project
+# from fabric.api import run, env, cd, sudo
+
+
+# def production():
+#     env.hosts = ['yourhost.com']
+#     env.repo = ('/srv/yourproject/', 'origin', 'master')
+
+
+# def git_pull(hash=None):
+#     """Updates or resets the repository"""
+#     repo, parent, branch = env.repo
+#     if hash is None:
+#         hash = '%s/%s' % (parent, branch)
+#     with cd(repo):
+#         run('git fetch')
+#         run('git reset --hard %s' % hash)
+
+
+# def buildout():
+#     repo, parent, branch = env.repo
+#     with cd(repo):
+#         run('./bin/buildout')
+
+
+# def deploy(hash=None):
+#     repo, parent, branch = env.repo
+#     git_pull(hash)
+#     buildout()
+#     with cd(repo):
+#         run('./bin/django syncdb')
+#     restart_supervisor()
+
+
+# def reset(hash):
+#     deploy(hash)
+
+
+# def restart_nginx():
+#     sudo('/etc/init.d/nginx restart')
+
+
+# def restart_supervisor():
+#     sudo('supervisorctl restart all')
