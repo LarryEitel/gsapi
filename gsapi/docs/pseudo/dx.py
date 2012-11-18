cf3bb136774a0325ae405d8afeba2bf62b5660ec
shiv shankar dayal <shivshankar.dayal@gmail.com>
2012-11-18 18:22:34 +0530

    sample dx pseudocode and notes on how to document python modules automatically

--- gsapi/docs/pseudo/dx.py
+++ gsapi/docs/pseudo/dx.py
@@ -1,4 +1,86 @@
-Pyseudo Dx Features/Functions
+Pseudo Dx Features/Functions
+
+Create user Mary Bell
+    controllers.cnt.CreateUsr(MaryBell)
+    
+Create company Program Group
+    controllers.Cnt.CreateCmp(pgrp)
+    
+Mary Bell associates herself with Program Group
+    controllers.Cnt.Associate(to_oid = pggrp, from_oid = MaryBell)
+        UI must select the two oids and a set of pre-existing relations will be populated in a drop down list.
+        One of the values from this drop down must be picked.
+        
+        controllers.Dx.CreateDx(to_oid = pggrp, from_oid = MaryBell)
+            This function will create a Dx object between these two oid.
+        controllers.Dx.CreateDxRel(to_oid = pggrp, from_oid = MaryBell)
+            This function will create a new Dx object between these two depending on the relation.
+        
+        Now Associate will populate tos and froms of both the objects.
+        Now update tos of children of Mary Bell. In this case there are none.
+
+Create company GSNI
+    controllers.Cnt.CreateCmp(ni)
+
+Mary Bell associates Program Group with GSNI
+    controllers.Cnt.Associate(to_oid = ni, from_oid = pggrp)
+        UI must select the two oids and a set of pre-existing relations will be populated in a drop down list.
+        One of the values from this drop down must be picked.
+        
+        controllers.Dx.CreateDx(to_oid = ni, from_oid = pggrp)
+            This function will create a Dx object between these two oid.
+        controllers.Dx.CreateDxRel(to_oid = ni, from_oid = pggrp)
+            This function will create a new Dx object between these two depending on the relation.
+        
+        Now Associate will populate tos and froms of both the objects.
+        Now update tos of children of Program Group. In this case there is one child Mary Bell so its tos must be updated.
+
+Create user Sally
+    controllers.Cnt.CreateUsr(Sally)
+
+Create company Kirmse
+    controllers.Cnt.CreateCmp(Krmse)
+
+Sally associates Kirmse with GSNI
+    controllers.Cnt.Associate(to_oid = ni, from_oid = kirmse)
+        UI must select the two oids and a set of pre-existing relations will be populated in a drop down list.
+        One of the values from this drop down must be picked.
+        
+        controllers.Dx.CreateDx(to_oid = ni, from_oid = kirmse)
+            This function will create a Dx object between these two oid.
+        controllers.Dx.CreateDxRel(to_oid = ni, from_oid = kirmse)
+            This function will create a new Dx object between these two depending on the relation.
+        
+        Now Associate will populate tos and froms of both the objects.
+        Now update tos of children of Kirmse. However, Kirmse do not have any children.
+
+Sally creates area 104
+    controllers.Cnt.CreateCmp(104)
+    
+Sally associates 104 with Kirmse 
+    controllers.Cnt.Associate(to_oid = ni, from_oid = kirmse)
+        UI must select the two oids and a set of pre-existing relations will be populated in a drop down list.
+        One of the values from this drop down must be picked.
+        
+        controllers.Dx.CreateDx(to_oid = ni, from_oid = kirmse)
+            This function will create a Dx object between these two oid.
+        controllers.Dx.CreateDxRel(to_oid = ni, from_oid = kirmse)
+            This function will create a new Dx object between these two depending on the relation.
+        
+        Now Associate will populate tos and froms of both the objects.
+        Now update tos of children of Kirmse. However, Kirmse do not have any children.
+
+
+
+
+
+
+
+
+
+
+
+
 
 Create company kirmse
     controllers.Cnt.CreateCmpKirmse
