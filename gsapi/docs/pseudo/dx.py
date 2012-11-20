17893e98ffa18c459a6d5fe93b512eddb63e2abe
shiv shankar dayal <shivshankar.dayal@gmail.com>
2012-11-20 17:11:04 +0530

    notes in generic and updated dx

--- gsapi/docs/pseudo/dx.py
+++ gsapi/docs/pseudo/dx.py
@@ -10,23 +10,152 @@ Create company Program Group
 Create Dx Relation
     Client:
         Processes:
-            HTTP POST: /Dx
+            HTTP POST: /Dx/Create
                 data:   
-                    toOID:
-                    frOID:
-                    dxRelOID:
+                    toOID  :
+                    to_c   :
+                    toNam  :
+                    toNams :
+                    frOID  :
+                    fr_c   :
+                    frNam  :
+                    frNamS :
+                    
+
     API:
         Processes:
-            Validate 
+            Validate OAuth acces_token validity. If valid check login session. If valid proceed.
+            If login is not valid redirect to login page. If OAuth is not valid send the response status code indicating OAuth access token needds to be renewed. 
 
-            FUNCTION: controllers.dx.create
+            FUNCTION: views.dx.create
                 data:
+                    toOID  :
+                    to_c   :
+                    toNam  :
+                    toNams :
+                    frOID  :
+                    fr_c   :
+                    frNam  :
+                    frNamS :
+                Call controller function for creating Dx and DxRel and also pass data.
+                
+            FUNCTION: controllers.dx.Create
+                data:
+                    toOID:
+                    frOID:
+                    
+                    Check OIDs for presence in database. Pull docs. If Docs do not exist respond with a status code. With a possible message of session hijacking.
+                    Create Dx and DxRel objects and generate OID and other ids.
+                    Updating tos and froms of objects.
+                        While updating any of these objects if document is found to be locked retry after half seonds.
+                        If maximum no. of retries are over send status code to retry in some time.
+                    If everything is good send status code to caller.
+                    
+Delete Dx relation
+    Client:
+        Processes:
+            HTTP POST: /Dx/Delete
+                data:
+                    OID:
+    API:
+        Processes:
+            Validate OAuth acces_token validity. If valid check login session. If valid proceed.
+            If login is not valid redirect to login page. If OAuth is not valid send the response status code indicating OAuth access token needds to be renewed. 
 
+            FUNCTION: views.dx.delete
+                data:
+                    OID:
+                
+                Call controller function to delete Dx and DxRel record.
+                
+            FUNCTION: controllers.dx.delete
+                data:
+                    OID:
+                    
+                    Check OID for presence in database. If Doc doe not exist respond with a status code. With a possible message of session hijacking.
+                    Call function deleteChild with frOID.
+                    
+            FUNCTION: contoller.dx.delete
+                    This function checks for children recursively. If they have multiple tos then the tos of this node and
+                    its childre is updated and child and its chilredn are not deleted.
+                    If any child has only one tos path leading to parent in consideration then that child and its Dx and DxRel objects are deleted.
+                        While deleting any of these objects if document is found to be locked retry after half seonds.
+                        If maximum no. of retries are over send status code to retry in some time.
+                    After deleting all Dx and DxRel record update parents' froms. 
+                    If everything is good send status code to caller.
+
+Delete Dx relation
+    Client:
+        Processes:
+            HTTP POST: /Dx/Update
+                data:
+                    OID:
+
+    API:
+        Processes:
+            Validate OAuth acces_token validity. If valid check login session. If valid proceed.
+            If login is not valid redirect to login page. If OAuth is not valid send the response status code indicating OAuth access token needds to be renewed. 
+
+            FUNCTION: views.dx.udpate
+                data:
+                    OID:
+            
+            
+            FUNCTION: controllers.dx.update
+                data:
+                    OID:
+                    
+                    Check OID for presence in database. If Doc doe not exist respond with a status code. With a possible message of session hijacking.
+                    Client will send request with type and value because any of the attributes can be updated. Any of the following can trigger a
+                    change in Dx and DxRel.
+                    fr_c, frNam, frNamS, frGen, to_c, toNam, toNamS, toGen, fam, mask, w.
+                    Changes in dNam and dNamS of objects will also trigger these changes. Gender change will also effect and class changes too.
+                    Check if these attributes have changed. If yes immediate parent's tos will be updated.
+                    And like deleteChild call update child with OID.
+                
+            FUNCTION: controllers.dx.updateChild
+                data:
+                    OID:
+                    
+                    Update childrens to path recursively.
+                        While updating any of these objects if document is found to be locked retry after half seonds.
+                        If maximum no. of retries are over send status code to retry in some time.
+                    Update all children and their tos and froms. 
+                    If everything is good send status code to caller.
+                    
+Move an object for new relation
+    Client:
+        Processes:
+            HTTP POST: /Dx/Move
+                data:
+                    old_toOID  :
+                    old_to_c   :
+                    old_toNam  :
+                    old_toNams :
+                    old_frOID  :
+                    old_fr_c   :
+                    old_frNam  :
+                    old_frNamS :
+                    new_toOID  :
+                    new_to_c   :
+                    new_toNam  :
+                    new_toNams :
+                    new_frOID  :
+                    new_fr_c   :
+                    new_frNam  :
+                    new_frNamS :
+                    
+        Comment: A move is basically a delete and create.
+
+                    
+    dRelTitle 
+    dRelDesc
 views.dx.Create
 views.dx.Create
 controllers.dx.Create
 controllers.dx.Delete
 controllers.dx.Update
+controllers.dx.Move
 
 
 Mary Bell associates herself with Program Group
