2c57f5d799e82bf7fb61e87accaf460ee1550c89
larryeitel <larry@eitel.com>
2012-11-27 07:44:28 -0600

    new utils directory

--- gsapi/tests/controllers/test_generic_mongo.py
+++ gsapi/tests/controllers/test_generic_mongo.py
@@ -18,51 +18,9 @@ from bson import json_util
 import models
 import controllers
 
-class TestGeneric(MongoTestCase):
+class TestGenericMongo(MongoTestCase):
     print "Generic tests"
     print "=============="
-    _c = 'Prs'
-
-    def test_post_nextId(self):
-        print
-        print "Post nextId"
-        print "^^^^^^^^^^^^"
-
-        db = self.db
-        generic = controllers.Generic(db)
-
-        # here is the basic function call being tested
-        fn = "controllers.generic.get(db, **args)"
-
-        # POST ONE ################################
-        host = self.host
-        sample_doc = {
-            "fNam":"johnathan",
-            "lNam":"doe",
-            "mOn": isodate.parse_datetime("2012-09-27T21:43:33.927Z"),
-            "oBy": ObjectId("50468de92558713d84b03fd0"),
-            "rBy": ObjectId("50468de92558713d84b03fd7"),
-            "gen":'m',
-            "emails" : [{
-                "email" : "john@doe.com"
-            }]
-        }
-
-        args = {}
-        args['_c'] = self._c
-        args['docs'] = [sample_doc]
-
-        response = generic.post(**args)
-
-        assert response['status_code'] == 200
-        data = response['response']
-        got_docs = data['docs']
-
-        assert data['total_inserted'] == 1
-
-        doc = data['docs'][0]['doc']
-        id = data['docs'][0]['id']
-        assert doc['fNam'] == sample_doc['fNam']
 
     def test_post(self):
         print
@@ -90,7 +48,6 @@ class TestGeneric(MongoTestCase):
         }
 
         args = {}
-        args['_c'] = self._c
         args['docs'] = [sample_doc]
 
         response = generic.post(**args)
